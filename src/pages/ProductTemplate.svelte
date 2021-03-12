cbf6b2ebc73fff45b446139be1e7dcac71f72ac9
Dariusz Franczak <darius32@op.pl>
2021-03-12 14:41:00 +0000

    single product page done

--- src/pages/ProductTemplate.svelte
+++ src/pages/ProductTemplate.svelte
@@ -1,6 +1,37 @@
 <script>
+  import { Link } from 'svelte-routing';
   // props
   export let id;
+
+  import defaultProductsStore from '../stores/defaultProductsStore';
+  import Loading from '../components/Loading.svelte';
+  import Products from '../components/Products/Products.svelte';
+
+  $: product = $defaultProductsStore.find(
+    (product) => product.id === parseInt(id)
+  );
 </script>
 
-<h1>product template page with id: {id}</h1>
+{#if !product}
+  <Loading />
+{:else}
+  <section class="single-product">
+    <Link class="btn btn-primary" to="/products">back to products</Link>
+    <div class="single-product-container">
+      <article class="single-product-image">
+        <img src={product.image} alt={product.title} />
+      </article>
+      <article>
+        <h1>{product.title}</h1>
+        <h2>{product.price}</h2>
+        <p>{product.description}</p>
+        <button
+          class="btn btn-primary btn-block"
+          on:click={() => {
+            console.log('add to card');
+          }}>add to cart</button
+        >
+      </article>
+    </div>
+  </section>
+{/if}
